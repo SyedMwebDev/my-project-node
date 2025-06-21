a822d041c01664f3e1a79e36a19da4b9e7341aa6
Syed MwebDev <muhammadbinalisyed@gmail.com>
2025-06-21 07:19:22 -0700

    Auto-create public folder in merge.js

--- merge.js
+++ merge.js
@@ -1,10 +1,22 @@
+// merge.js
+import fs from 'fs';
+import path from 'path';
 import PDFMerger from 'pdf-merger-js';
 
-const pdfMerger = async (p1, p2) => {
-    const merger = new PDFMerger();
-    await merger.add(p1);
-    await merger.add(p2);
-    await merger.save('public/merged.pdf');
+const pdfMerger = async (inputPath1, inputPath2) => {
+  // Ensure the public folder exists before writing
+  const publicDir = path.join(process.cwd(), 'public');
+  if (!fs.existsSync(publicDir)) {
+    fs.mkdirSync(publicDir, { recursive: true });
+  }
+
+  const merger = new PDFMerger();
+  await merger.add(inputPath1);
+  await merger.add(inputPath2);
+
+  // Use absolute path to save
+  const outputPath = path.join(publicDir, 'merged.pdf');
+  await merger.save(outputPath);
 };
 
 export { pdfMerger };
